175b78a5ff059416e035d5dfec197f1f951777d9
JerryWski <jarek_wasilewski@yahoo.com>
2023-01-13 19:20:46 +0100

    js part - logic

---
+++ main.js
@@ -0,0 +1,14 @@
+const amountInpt = document.querySelector('#amount');
+const rateInput = document.querySelector('#rate');
+const monthInput = document.querySelector('#month');
+const btnDeposit = document.querySelector('.calcuate-btn-depo');
+const btnInvestment = document.querySelector('.calcuate-btn-invest');
+const interestAmountDepo = document.querySelector('.total-interest-depo');
+const totalAmountDepo = document.querySelector('.total-amount-depo');
+const interestAmountInvest = document.querySelector('.total-interest-invest');
+const totalAmountInvest = document.querySelector('.total-amount-invest');
+
+
+
+
+
