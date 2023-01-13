a0b4249475a0f448b05721ff18fa1902da7759cd
JerryWski <jarek_wasilewski@yahoo.com>
2023-01-13 20:14:31 +0100

    js continue

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
-const amountInpt = document.querySelector('#amount');
-const rateInput = document.querySelector('#rate');
-const monthInput = document.querySelector('#month');
-const btnDeposit = document.querySelector('.calcuate-btn-depo');
-const btnInvestment = document.querySelector('.calcuate-btn-invest');
-const interestAmountDepo = document.querySelector('.total-interest-depo');
-const totalAmountDepo = document.querySelector('.total-amount-depo');
-const interestAmountInvest = document.querySelector('.total-interest-invest');
-const totalAmountInvest = document.querySelector('.total-amount-invest');
+let amountInput = document.querySelector("#amount");
+let rateInput = document.querySelector("#rate");
+let monthInput = document.querySelector("#month");
+const btnDeposit = document.querySelector(".calcuate-btn-depo");
+const btnInvestment = document.querySelector(".calcuate-btn-invest");
+const interestAmountDepo = document.querySelector(".total-interest-depo");
+const totalAmountDepo = document.querySelector(".total-amount-depo");
+const interestAmountInvest = document.querySelector(".total-interest-invest");
+const totalAmountInvest = document.querySelector(".total-amount-invest");
 
+let interest;
 
+let calculateDepo = () => {
+    
+  let principal = amountInput.value;
+  let rate = rateInput.value / 100 / 12;
+  let month = monthInput.value;
+  
+  if(month == 1) {
+    interest = (principal * rate * month )* 12;
+    console.log(interest);
+  }else if (month == 2) {
+    interest = (principal * rate * month )* 6;
+    console.log(interest);
+  }else {
+    console.log('wprowadz poprawny okres');
+    return
+  }
+};
 
-
-
+btnDeposit.addEventListener("click", calculateDepo);
