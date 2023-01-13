7bc350134d45e6aed78cc2a93521f9531bb2c73e
JerryWski <jarek_wasilewski@yahoo.com>
2023-01-13 22:40:49 +0100

    shorter code

--- main.js
+++ main.js
@@ -8,62 +8,18 @@ const interestAmountDepo = document.querySelector(".total-interest-depo");
 const totalAmountDepo = document.querySelector(".total-amount-depo");
 const interestAmountInvest = document.querySelector(".total-interest-invest");
 const totalAmountInvest = document.querySelector(".total-amount-invest");
+let interest;
 
 let calculateDepo = () => {
   let principal = amountInput.value;
   let rate = (rateInput.value / 100);
   let month = monthInput.value;
 
-  let interest;
-  if (month == 1) {
-    interest = (principal * rate) * (month / 12);
+  if(month >= 1 && month <= 12) {
+    interest = (principal * rate) * (month / 12)
     console.log(interest);
-  } else if (month == 2) {
-    interest = (principal * rate) * (month / 12);
-    console.log(interest);
-  }else if(month == 3) {
-    interest = (principal * rate) * (month / 12);
-    console.log(interest);
-  }else if (month == 4) {
-    interest = (principal * rate) * (month / 12);
-    console.log(interest);
-  }
-  else if (month == 5) {
-    interest = (principal * rate) * (month / 12);
-    console.log(interest);
-  }
-  else if (month == 6) {
-    interest = (principal * rate) * (month / 12);
-    console.log(interest);
-  }
-  else if (month == 7) {
-    interest = (principal * rate) * (month / 12);
-    console.log(interest);
-  }
-  else if (month == 8) {
-    interest = (principal * rate) * (month / 12);
-    console.log(interest);
-  }
-  else if (month == 9) {
-    interest = (principal * rate) * (month / 12);
-    console.log(interest);
-  }
-  else if (month == 10) {
-    interest = (principal * rate) * (month / 12);
-    console.log(interest);
-  }
-  else if (month == 11) {
-    interest = (principal * rate) * (month / 12);
-    console.log(interest);
-  }
-  else if (month == 12) {
-    interest = (principal * rate) * (month / 12);
-    console.log(interest);
-  }
-  else {
+  } else {
     console.log("enter a range of 1 to 12 months");
-    
-  }
-};
-
+    }
+}
 btnDeposit.addEventListener("click", calculateDepo);
