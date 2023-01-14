ef18c1a62deb0718b54df64569420b0b0c7cd795
JerryWski <jarek_wasilewski@yahoo.com>
2023-01-14 19:43:28 +0100

    error text code

--- main.js
+++ main.js
@@ -10,9 +10,11 @@ const interestAmountDepo = document.querySelector(".total-interest-depo");
 const totalAmountDepo = document.querySelector(".total-amount-depo");
 const interestAmountInvest = document.querySelector(".total-interest-invest");
 const totalAmountInvest = document.querySelector(".total-amount-invest");
+//error text
+const errorText = document.querySelector('.error-text')
 let interest;
 
-let calculateDepo = () => {
+const calculateDepo = () => {
   let principal = amountInput.value;
   let rate = (rateInput.value / 100);
   let month = monthInput.value;
@@ -21,8 +23,15 @@ let calculateDepo = () => {
     interest = (principal * rate) * (month / 12);
     interestAmountDepo.textContent = interest.toFixed(2);
     totalAmountDepo.textContent = principal + interest.toFixed(2);
+    clearError();
   } else {
-    console.log("enter a range of 1 to 12 months");
+    errorText.textContent = " Please enter a range of 1 to 12 months";
+    
     }
 }
+
+const clearError = () => {
+  errorText.textContent = '';
+}
+
 btnDeposit.addEventListener("click", calculateDepo);
