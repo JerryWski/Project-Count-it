fd344050144fb2ef1cef24ca02fe84ab8a3287ff
JerryWski <jarek_wasilewski@yahoo.com>
2023-01-14 19:34:34 +0100

    interest and total amount code

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+//qs for data inputs
 const amountInput = document.querySelector("#amount");
 const rateInput = document.querySelector("#rate");
 const monthInput = document.querySelector("#month");
-
+//buttons for counting deposit and investment
 const btnDeposit = document.querySelector(".calcuate-btn-depo");
 const btnInvestment = document.querySelector(".calcuate-btn-invest");
+//amounts for deposit and investment
 const interestAmountDepo = document.querySelector(".total-interest-depo");
 const totalAmountDepo = document.querySelector(".total-amount-depo");
 const interestAmountInvest = document.querySelector(".total-interest-invest");
@@ -16,8 +18,9 @@ let calculateDepo = () => {
   let month = monthInput.value;
 
   if(month >= 1 && month <= 12) {
-    interest = (principal * rate) * (month / 12)
-    console.log(interest);
+    interest = (principal * rate) * (month / 12);
+    interestAmountDepo.textContent = interest.toFixed(2);
+    totalAmountDepo.textContent = principal + interest.toFixed(2);
   } else {
     console.log("enter a range of 1 to 12 months");
     }
