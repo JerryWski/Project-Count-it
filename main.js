6455bcaacc19dae2a11e27a78bc95ad7cadecc35
JerryWski <jarek_wasilewski@yahoo.com>
2023-01-16 17:58:38 +0100

    fixes for js logic

--- main.js
+++ main.js
@@ -11,41 +11,46 @@ const totalAmountDepo = document.querySelector(".total-amount-depo");
 const interestAmountInvest = document.querySelector(".total-interest-invest");
 const totalAmountInvest = document.querySelector(".total-amount-invest");
 //error text
-const errorText = document.querySelector('.error-text')
+const errorText = document.querySelector(".error-text");
 
 let interest;
 let interestTwo;
 
 const calculateDepo = () => {
   let principal = amountInput.value;
-  let rate = (rateInput.value / 100);
+  let rate = rateInput.value / 100;
   let month = monthInput.value;
 
-  if(month >= 1 && month <= 12) {
-    interest = (principal * rate * 0.81) * (month / 12);
+  if (month >= 1 && month <= 12) {
+    interest = principal * rate * 0.81 * (month / 12);
     interestAmountDepo.textContent = interest.toFixed(2);
-    totalAmountDepo.textContent = parseFloat(principal) + parseFloat(interest.toFixed(2));
+    totalAmountDepo.textContent =
+      parseFloat(principal) + parseFloat(interest.toFixed(2));
     clearError();
-    
   } else {
     errorText.textContent = " Please enter a numbers from 1 to 12";
-    
   }
-}
+};
 
 const calcuateInvest = () => {
   let principal = amountInput.value;
-  let rate = (rateInput.value / 100);
+  let rate = rateInput.value / 100;
   let month = monthInput.value;
-
-  interestTwo = (principal * rate * 0.81) * month;
-  interestAmountInvest.textContent = interestTwo.toFixed(2);
-  totalAmountInvest.textContent = parseFloat(principal) + parseFloat(interestTwo.toFixed(2))
-}
+  
+  if (month >= 1 && month <= 12) {
+    interestTwo = principal * rate * 0.81;
+    interestAmountInvest.textContent = interestTwo.toFixed(2);
+    totalAmountInvest.textContent =
+      parseFloat(principal) + parseFloat(interestTwo.toFixed(2));
+      clearError();
+  }else {
+    errorText.textContent = " Please enter a numbers from 1 to 12";
+  }
+};
 
 const clearError = () => {
-  errorText.textContent = '';
-}
+  errorText.textContent = "";
+};
 
 btnDeposit.addEventListener("click", calculateDepo);
-btnInvestment.addEventListener('click', calcuateInvest);
+btnInvestment.addEventListener("click", calcuateInvest);
