e8fe5c3b158d66b1f4f99c74f1dc93d2e54f4969
JerryWski <jarek_wasilewski@yahoo.com>
2023-01-30 19:07:04 +0100

    removing old code and polishing

--- main.js
+++ main.js
@@ -16,71 +16,6 @@ const errorTextAmount = document.querySelector(".error-text-amount");
 const amountTextDepo = document.querySelectorAll(".amount-text-depo");
 const amountTextInvest = document.querySelectorAll(".amount-text-invest");
 
-// let interest;
-// let interestTwo;
-
-// const checkInputValues = () => {
-//   if (amountInput.value <= 0 || rateInput.value <= 0) {
-//     errorTextAmount.textContent = "Please enter a number greater than 0";
-//     return false;
-//   }
-//   errorTextAmount.textContent= '';
-//   return true;
-// };
-
-// const calculateDepo = () => {
-//   let principal = amountInput.value;
-//   let rate = rateInput.value / 100;
-//   let month = monthInput.value;
-
-//   if (month >= 1 && month <= 12) {
-//     interest = principal * rate * 0.81 * (month / 12);
-//     amountTextDepo.forEach((text) => {
-//       text.classList.add("active");
-//     });
-//     interestAmountDepo.textContent = interest.toFixed(2);
-//     totalAmountDepo.textContent =
-//       parseFloat(principal) + parseFloat(interest.toFixed(2));
-//     clearError();
-//   } else {
-//     errorTextDuration.textContent = " Please enter a numbers from 1 to 12";
-//   }
-// };
-
-// const calcuateInvest = () => {
-//   let principal = amountInput.value;
-//   let rate = rateInput.value / 100;
-//   let month = monthInput.value;
-
-//   if (month >= 1 && month <= 12) {
-//     interestTwo = principal * rate * 0.81;
-//     amountTextInvest.forEach((text) => {
-//       text.classList.add("active");
-//     });
-//     interestAmountInvest.textContent = interestTwo.toFixed(2);
-//     totalAmountInvest.textContent =
-//       parseFloat(principal) + parseFloat(interestTwo.toFixed(2));
-//     clearError();
-//   } else {
-//     errorTextDuration.textContent = " Please enter a numbers from 1 to 12";
-//   }
-// };
-
-// const clearError = () => {
-//   errorTextDuration.textContent = "";
-// };
-
-// btnDeposit.addEventListener("click", () => {
-//   if (checkInputValues()) {
-//     calculateDepo();
-//   }
-// });
-// btnInvestment.addEventListener("click", () => {
-//   if (checkInputValues()) {
-//     calcuateInvest();
-//   }
-// });
-
 const calculate = (e) => {
   let principal = amountInput.value;
   let rate = rateInput.value / 100;
@@ -90,8 +25,8 @@ const calculate = (e) => {
   if (principal <= 0 || rate <= 0) {
     errorTextAmount.textContent = "Please enter a number greater than 0";
     return;
-  }else {
-    errorTextAmount.textContent= '';
+  } else {
+    errorTextAmount.textContent = "";
   }
 
   if (month >= 1 && month <= 12) {
